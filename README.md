018b75b3408add97d901d31ccce4027de9ea414f
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-02-07 18:51:05 +0200

    [1.0.0] added wsl paths

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ A collection of custom libraries that I use in many of my Python projects!
 - `JsonHandler`: 
 - `StringParser`: 
 - `BookmarksGetter`: 
+- `wsl_paths`: functions for handling conversion between Windows and WSL filepaths
 
 ## Libraries to add:
 - `sqlite_api`: 
--- src/handymatt/__init__.py
+++ src/handymatt/__init__.py
@@ -2,5 +2,6 @@
 from .string_parser import StringParser
 from .json_handler import JsonHandler
 from .bookmarks_getter import BookmarksGetter
+import handymatt.wsl_paths as wsl_paths
 
-__all__ = ["StringParser", "JsonHandler", "BookmarksGetter"]
+__all__ = ["StringParser", "JsonHandler", "BookmarksGetter", "wsl_paths"]
