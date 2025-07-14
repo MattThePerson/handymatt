dc335dda47f560f5663296e44573ee3db16d08a4
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-07-14 17:13:00 +0300

    [1.2.1] Removed WSL bandaid from BookmarksGetter

--- src/handymatt/bookmarks_getter.py
+++ src/handymatt/bookmarks_getter.py
@@ -1,5 +1,5 @@
 from typing import Any
-#import os
+import os
 import json
 import datetime
 
@@ -14,8 +14,8 @@ class BookmarksGetter:
     '''
     DEFUALT_BOOKMARKS_PATHS = [
         r'%localappdata%\Google\Chrome\User Data\Default\Bookmarks',
-        '/mnt/c/Users/stirl/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/Bookmarks' # WSL bandaid
-        # r'%localappdata%\BraveSoftware\Brave-Browser\User Data\Default\Bookmarks',
+        r'%localappdata%\BraveSoftware\Brave-Browser\User Data\Default\Bookmarks',
+        # '/mnt/c/Users/stirl/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/Bookmarks' # WSL bandaid
     ]
 
     @classmethod
@@ -27,7 +27,7 @@ class BookmarksGetter:
         if bookmarks_path == None:
             print('Cannot find bookmarks for browser named "{}"'.format(browser))
             return None
-        # bookmarks_path = bookmarks_path.replace(r'%localappdata%', os.getenv('LOCALAPPDATA')) #type:ignore
+        bookmarks_path = bookmarks_path.replace(r'%localappdata%', os.getenv('LOCALAPPDATA', 'None'))
         with open(bookmarks_path, 'r') as f:
             bookmarks_json = json.load(f)
         if browser.lower() not in 'chrome chromium bravesoftware edge':
